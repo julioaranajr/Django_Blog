43943b20b67f6bad1b626f91ece53e49c5679f54
Julio Arana Jr. <julioarana.de@gmail.com>
2024-08-25 16:38:11 +0200

    ft. templates and passing some dummy data to the html templates

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
 
+post = [
+    {
+        'author': 'JulioAranaJr',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'August 25, 2024'
+    },
+    {
+        'author': 'Nizar',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'August 25, 2024'
+    }
+]
 
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': post
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
     return render(request, 'blog/about.html')
