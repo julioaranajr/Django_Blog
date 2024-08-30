c9a4d41ebc24646e003d93f59465c579c017ed1b
Julio Arana Jr. <julioarana.de@gmail.com>
2024-08-30 23:17:27 +0200

    ft. create, update and delete posts

--- django_project/django_project/urls.py
+++ django_project/django_project/urls.py
@@ -21,6 +21,7 @@ from users import views as user_views
 from django.conf import settings
 from django.conf.urls.static import static
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', user_views.register, name='register'),
